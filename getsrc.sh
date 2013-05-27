fb8cadff5aaeb38eeb31a1c397c42130e7bc6af0
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2013-05-26 21:44:23 -0700

    Forgot a library in Hangouts

--- getsrc.sh
+++ getsrc.sh
@@ -4,3 +4,4 @@ git clone https://github.com/zhuowei/Xenologer-src-glasshome.git glasshome
 git clone https://github.com/zhuowei/Xenologer-src-glasscamera.git glasscamera
 git clone https://github.com/zhuowei/Xenologer-src-glasssetup.git glasssetup
 git clone https://github.com/zhuowei/Xenologer-src-glassmaps.git glassmaps
+git clone https://github.com/zhuowei/Xenologer-src-glasshangouts.git glasshangouts
